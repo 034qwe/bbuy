9be746fe966313948106174b491fab4b66b839e1
bohdan <johndoe@example.com>
2024-11-20 20:53:20 +0200

    first models

--- bbuyshop/main/admin.py
+++ bbuyshop/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+
+from .models import *
+
+admin.register(Menu)
+admin.register(Ingredient)
 # Register your models here.
--- bbuyshop/main/models.py
+++ bbuyshop/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField('name',max_length=100),
+    inside = models.ManyToManyField('ingredient',)
+
+
+
+class Ingredient(models.Model):
+    how_much = models.IntegerField()
+    name_ing = models.CharField(max_length=300)
